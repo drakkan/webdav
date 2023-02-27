32996838bcd881c0a7d5812f23299ddc9ac9f110
Kyle Larose <kyle@agilicus.com>
2023-02-27 18:53:13 +0100

    do not bail out on more filesystem errors
    There are more places that the file system can return errors we weren't
    properly handling. I.e. if the stat returns a failure when walking a
    directory, we may not have properly skipped a given file.

--- file.go
+++ file.go
@@ -778,7 +778,7 @@ func walkFS(ctx context.Context, fs FileSystem, depth int, name string, info os.
 	// This implementation is based on Walk's code in the standard path/filepath package.
 	err := walkFn(name, info, nil)
 	if err != nil {
-		if info.IsDir() && err == filepath.SkipDir {
+		if err == filepath.SkipDir {
 			return nil
 		}
 		return err
@@ -805,7 +805,7 @@ func walkFS(ctx context.Context, fs FileSystem, depth int, name string, info os.
 		filename := path.Join(name, fileInfo.Name())
 		err = walkFS(ctx, fs, depth, filename, fileInfo, walkFn)
 		if err != nil {
-			if !fileInfo.IsDir() || err != filepath.SkipDir {
+			if err != filepath.SkipDir {
 				return err
 			}
 		}
